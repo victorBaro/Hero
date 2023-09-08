4a6b6314817b21525490c2ad791ed7960dfc1607
Victor Baro <fs.baro@gmail.com>
2023-09-08 16:33:08 -0700

    Remove UITabBarControllerDelegate

--- Sources/Transition/HeroTransition+UITabBarControllerDelegate.swift
+++ Sources/Transition/HeroTransition+UITabBarControllerDelegate.swift
@@ -20,36 +20,36 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#if canImport(UIKit)
+// #if canImport(UIKit)
 
-import UIKit
+// import UIKit
 
-extension HeroTransition: UITabBarControllerDelegate {
-  public func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-    guard tabBarController.selectedViewController !== viewController else {
-      return false
-    }
-    if isTransitioning {
-      cancel(animate: false)
-    }
-    return true
-  }
+// extension HeroTransition: UITabBarControllerDelegate {
+//   public func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+//     guard tabBarController.selectedViewController !== viewController else {
+//       return false
+//     }
+//     if isTransitioning {
+//       cancel(animate: false)
+//     }
+//     return true
+//   }
 
-  public func tabBarController(_ tabBarController: UITabBarController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-    return interactiveTransitioning
-  }
+//   public func tabBarController(_ tabBarController: UITabBarController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+//     return interactiveTransitioning
+//   }
 
-  public func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-    guard !isTransitioning else { return nil }
-    self.state = .notified
-    let fromVCIndex = tabBarController.children.firstIndex(of: fromVC)!
-    let toVCIndex = tabBarController.children.firstIndex(of: toVC)!
-    self.isPresenting = toVCIndex > fromVCIndex
-    self.fromViewController = fromViewController ?? fromVC
-    self.toViewController = toViewController ?? toVC
-    self.inTabBarController = true
-    return self
-  }
-}
+//   public func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+//     guard !isTransitioning else { return nil }
+//     self.state = .notified
+//     let fromVCIndex = tabBarController.children.firstIndex(of: fromVC)!
+//     let toVCIndex = tabBarController.children.firstIndex(of: toVC)!
+//     self.isPresenting = toVCIndex > fromVCIndex
+//     self.fromViewController = fromViewController ?? fromVC
+//     self.toViewController = toViewController ?? toVC
+//     self.inTabBarController = true
+//     return self
+//   }
+// }
 
-#endif
+// #endif
